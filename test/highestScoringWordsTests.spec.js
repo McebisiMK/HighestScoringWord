bd25ad30d2e5e3218f9357588474f59918b693cd
Mcebisi Mkhohliwe <McebisiM@SAHOMELOANS.COM>
2018-06-07 16:08:16 +0200

    still working on it

--- test/highestScoringWordsTests.spec.js
+++ test/highestScoringWordsTests.spec.js
@@ -1,10 +1,50 @@
 describe("HighestScoringWord", function () {
     describe("getHighestScoringWord", function () {
-        describe("Given a string of words'", function () {
-            [{ words: "hello is me", output: "hello" },
-            { words: "beat it now", output: "now" },
-            { words: "i love you", output: "you" },
-            { words: "the big bad wolf ate the fat chicken", output: "wolf" },
+        describe("Given a single word", function () {
+            [{ word: "hello", output: "hello" },
+            { word: "now", output: "now" },
+            { word: "you", output: "you" }]
+                .forEach(function (element) {
+                    xit("should return that word as highest scoring word ", function () {
+                        //----------------Arrange------------
+                        let highestScoringWord = new HighestScoringWord();
+                        let word = element.word;
+                        //----------------Act----------------
+                        let actual = highestScoringWord.getHighestScoringWord(word);
+
+                        //----------------Assert-------------
+                        let expected = element.output;
+                        expect(actual).toBe(expected);
+
+                    })
+                })
+
+        })
+
+        describe("Given a string of two words'", function () {
+            [{ words: "my name", output: "my" },
+            { words: "beat  now", output: "now" },
+            { words: "planet earth", output: "planet" }]
+                .forEach(function (element) {
+                    xit("should return highest scoring word ", function () {
+                        //----------------Arrange------------
+                        let highestScoringWord = new HighestScoringWord();
+                        let sentence = element.words;
+                        //----------------Act----------------
+                        let actual = highestScoringWord.getHighestScoringWord(sentence);
+
+                        //----------------Assert-------------
+                        let expected = element.output;
+                        expect(actual).toBe(expected);
+
+                    })
+                })
+
+        })
+
+        describe("Given a string of many words'", function () {
+            [//{ words: "the big bad wolf ate the fat chicken", output: "wolf" },
+            // { words: "monday tuesday wednesday thursday friday", output: "thursday" },
             { words: "polo bears live on ice", output: "polo" }]
                 .forEach(function (element) {
                     it("should return highest score word ", function () {
